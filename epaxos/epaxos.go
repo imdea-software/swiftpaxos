bbbe6fcc61bf987d91afc49a379af62906f7e784
otrack <0track@gmail.com>
2026-03-02 21:56:18 +0100

    update

--- epaxos/epaxos.go
+++ epaxos/epaxos.go
@@ -246,7 +246,7 @@ func (r *Replica) fastClock() {
 }
 func (r *Replica) slowClock() {
 	for !r.Shutdown {
-		time.Sleep(150 * time.Millisecond)
+		time.Sleep(500 * time.Millisecond)
 		r.Printf("weird %d; conflicted %d; slow %d; fast %d\n", r.Stats.M["weird"], r.Stats.M["conflicted"], r.Stats.M["slow"], r.Stats.M["fast"])
 		slowClockChan <- true
 	}
